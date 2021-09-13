d6c56ba11428c9f07f148e75fdd7c371df14bd13
WaerWaters <daniel.johnsen1@hotmail.com>
2021-09-13 13:00:53 +0200

    finished javascript to dropdown menu

--- dropdownMenu.js
+++ dropdownMenu.js
@@ -1,1 +1,21 @@
 
+
+// when the user clicks on the button, toggle between hiding and showing the dropdown content
+function dropFunction() {
+    document.getElementById("dropdown").classList.toggle("show");
+}
+
+// close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content")
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show')
+            }
+        }
+    }
+}
+
